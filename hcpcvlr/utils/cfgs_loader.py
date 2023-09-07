6d53e5d482aebada7cfcf37611ec82dfbdb0ea4d
WissingChen <chen867820261@163.com>
2023-09-07 23:30:43 +0800

    code update

--- hcpcvlr/utils/cfgs_loader.py
+++ hcpcvlr/utils/cfgs_loader.py
@@ -1,16 +1,20 @@
 import yaml
 
 
-def load_yaml(yaml_path):
+def load_yaml(yaml_path, cfgs=None):
     """
     loading config file .yaml
     and extend the dict.
     """
-    cfgs = {}
+    if cfgs is None:
+        cfgs = {}
     with open(yaml_path, 'r', encoding='utf-8') as f:
         _cfgs = yaml.load(f.read(), Loader=yaml.FullLoader)
         for key in _cfgs.keys():
-            cfgs.update(_cfgs[key])
+            if key == '__base__':
+                cfgs = load_yaml(_cfgs[key], cfgs)
+            else:
+                cfgs.update(_cfgs[key])
     return cfgs
 
 def load_json(json_path):
